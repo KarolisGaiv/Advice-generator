5393a59905ccbefc19c3061d8a367280c2b8e947
KarolisGaiv <kgaivenis@gmail.com>
2022-03-05 13:55:24 +0200

    Load advice text, add divider image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './styles/main.scss';
 import { useEffect, useState } from 'react';
+import MobileDivider from './images/pattern-divider-mobile.svg';
 
 function App() {
   const [advice, setAdvice] = useState('');
 
   useEffect(() => {
     fetchData();
-  });
+  }, []);
 
   const fetchData = async () => {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
     setAdvice(data);
-    console.log(advice);
+    console.log(data);
   };
 
   return (
     <div className='advice-container'>
       <h1 className='advice-container__header'>Advice #{advice.slip.id}</h1>
+      <p className='advice-container__content'>"{advice.slip.advice}"</p>
+      <img src={MobileDivider} alt='' />
     </div>
   );
 }

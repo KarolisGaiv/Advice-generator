1ac366592acda927138aa4018977c94b426a49f7
KarolisGaiv <kgaivenis@gmail.com>
2022-03-05 13:09:12 +0200

    Add header, add advice fetch funcionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './styles/main.scss';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return <div className='App'>test</div>;
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  const fetchData = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data);
+    console.log(advice);
+  };
+
+  return (
+    <div className='advice-container'>
+      <h1 className='advice-container__header'>Advice #{advice.slip.id}</h1>
+    </div>
+  );
 }
 
 export default App;

189de2dc7e560b3feb1a5dc01bfb93eb6f5d352d
KarolisGaiv <kgaivenis@gmail.com>
2022-03-06 10:01:39 +0200

    Display divider line based on screen width

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import './styles/main.scss';
 import { useEffect, useState } from 'react';
 import Dice from './images/icon-dice.svg';
 import MobileDivider from './images/pattern-divider-mobile.svg';
+import DesktopDivider from './images/pattern-divider-desktop.svg';
 
 function App() {
   const [advice, setAdvice] = useState();
+  const [screenWidth, setScreenWidth] = useState();
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  // Render divider line based on current screen with
+  useEffect(() => {
+    window.addEventListener('resize', getScreenWidth);
+  }, [screenWidth]);
 
   const fetchData = async () => {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
     setAdvice(data);
   };
+
+  function getScreenWidth() {
+    setScreenWidth(window.innerWidth);
+  }
 
   function changeAdvice() {
     fetchData();
@@ -26,11 +37,20 @@ function App() {
     <div className='advice-container'>
       <h1 className='advice-container__header'>Advice #{advice.slip.id}</h1>
       <p className='advice-container__content'>"{advice.slip.advice}"</p>
-      <img
-        className='advice-container__divider'
-        src={MobileDivider}
-        alt='straight line divider'
-      />
+
+      {screenWidth > 1023 ? (
+        <img
+          src={DesktopDivider}
+          className='advice-container__divider'
+          alt='straight line divider'
+        />
+      ) : (
+        <img
+          src={MobileDivider}
+          className='advice-container__divider'
+          alt='straight line divider'
+        />
+      )}
       <button className='generate-btn' onClick={changeAdvice}>
         <img src={Dice} alt='' />
       </button>
